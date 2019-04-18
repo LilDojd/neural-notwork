2ca91b7ce914335c3c3664da0f1a24bbfce6d25d
LilDojd <yawner@protonmail.com>
2019-04-18 20:20:16 +0300

    Previous checkpoint system depricated

--- NN2019/extract.py
+++ NN2019/extract.py
@@ -345,10 +345,11 @@ if __name__ == '__main__':
     if args.mode == "extract":
         pdb_filenames = glob.glob(os.path.join(args.pdb_input_dir, "*.pdb"))
         to_pass = glob.glob(os.path.join(args.output_dir, "*.npz"))
-        to_pass = set(['_'.join(i.split('_')[0:2]) for i in to_pass])
-        pdb_filenames = [pdb for pdb in pdb_filenames if '_'.join(pdb.split('_')[1:3]) not in to_pass]
+        to_pass = set(['_'.join(os.path.basename(i).split('_')[0:2]) for i in to_pass])
+        pdb_filenames = [pdb for pdb in pdb_filenames if '_'.join(os.path.basename(pdb).split('_')[1:3]) not in to_pass]
         print("Previously extracted files from a whole set:")
-        print(['_'.join(pdb.split('_')[1:3]) for pdb in pdb_filenames], Bcolors.WARNING, to_pass, Bcolors.ENDC)
+        print(['_'.join(os.path.basename(pdb).split('_')[1:3]) for pdb in pdb_filenames],
+              Bcolors.WARNING, to_pass, Bcolors.ENDC)
         print("Iterating on unextracted files only")
         joblib.Parallel(n_jobs=args.n_proc, batch_size=1)(
             joblib.delayed(extract_atomistic_features)(pdb_filename,
